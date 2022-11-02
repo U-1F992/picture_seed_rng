9604a38ef792a799e26bd328039eaae486a935fe
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-11-02 22:36:54 +0900

    Add sample

--- picture_seed.py
+++ picture_seed.py
@@ -1,57 +1,121 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from Commands.PythonCommandBase import ImageProcPythonCommand
+from Commands.Keys import Hat
 
-from abc import ABCMeta
+from .gc_button_alias import Button
+from .picture_seed_util import WaitThread, execute
+from .pokecon_extension import execute_sequence, repeat
 
-from Commands.PythonCommandBase import PythonCommand, ImageProcPythonCommand
-from Commands.Keys import KeyPress, Button, Hat, Direction, Stick
+DEFAULT_DURATION = 0.1
 
-from .util import WaitThread, execute
-
-class OperationWithCommand(metaclass=ABCMeta):
-    """ImageProcPythonCommandを使用する操作定義の抽象基底クラス
+class Reset():
+    """リセットして、マルチブート待機までの操作を定義するクラス
     """
     def __init__(self, command: ImageProcPythonCommand):
         self.__command = command
     def run(self):
-        pass
+        execute_sequence(self.__command, [
+            # Zメニューを開く
+            (Button.Z, DEFAULT_DURATION, 2),
+            # カートリッジこうかん
+            (Button.A, DEFAULT_DURATION, 1),
+            # 「ゲームを終了してカートリッジを交換しますか？」「いいえ」
+            (Hat.LEFT, DEFAULT_DURATION, 1),
+            # 「はい」
+            (Button.A, DEFAULT_DURATION, 1),
+            # 「カートリッジを交換してください。」
+            (Button.A, DEFAULT_DURATION, 1),
+            ([Button.START, Button.X], 4, 2),
+        ])
 
-class Reset(OperationWithCommand):
-    """リセットして、マルチブート待機までの操作を定義するクラス
-    """
-    def run(self):
-        self.__command.wait(1)
-
-        self.__command.press(Button.ZR, 0.1, 2)
-        self.__command.press(Button.A, 0.1, 1)
-        self.__command.press(Hat.LEFT, 0.1, 1)
-        self.__command.press(Button.A, 0.1, 1)
-        self.__command.press(Button.A, 0.1, 1)
-        self.__command.press([Button.A, Button.B], 4, 1)
-
-class LoadGame(OperationWithCommand):
+class LoadGame():
     """マルチブート待ち受けを解除して、絵画鑑賞の直前までの操作を定義するクラス
     """
+    def __init__(self, command: ImageProcPythonCommand):
+        self.__command = command
     def run(self):
-        pass
+        execute_sequence(self.__command, [
+            # 待ち受け解除
+            (Button.A, DEFAULT_DURATION, 6),
+            # オープニングをスキップ
+            (Button.A, DEFAULT_DURATION, 1.5),
+            # タイトル表示までスキップ
+            (Button.A, DEFAULT_DURATION, 1.5),
+            # 「PUSH START BUTTON」
+            (Button.A, DEFAULT_DURATION, 3),
+            # 「でんちぎれの　ために　とけいが　うごかなくなりました」
+            (Button.A, DEFAULT_DURATION, 2),
+            # 「とけいに　かんけいする　できごとは　おきませんが　ゲームを　つづけて　あそぶことは　できます」
+            (Button.A, DEFAULT_DURATION, 2),
+            # セーブデータ選択
+            (Button.A, DEFAULT_DURATION, 2),
+        ])
 
-class SeePicture(OperationWithCommand):
+class SeePicture():
     """絵画を見て、離脱する操作を定義するクラス
     """
+    def __init__(self, command: ImageProcPythonCommand):
+        self.__command = command
     def run(self):
-        pass
+        execute_sequence(self.__command, [
+            # 絵画を見る
+            (Button.A, DEFAULT_DURATION, 2),
+            # 絵画画面から離脱する
+            (Button.A, DEFAULT_DURATION, 2),
+        ])
 
-class MoveToDestination(OperationWithCommand):
+class MoveToDestination():
     """絵画を見た直後から、エンカウントの直前まで移動する操作を定義するクラス
     """
+    def __init__(self, command: ImageProcPythonCommand):
+        self.__command = command
     def run(self):
-        pass
+        execute_sequence(self.__command, [
 
-class Encounter(OperationWithCommand):
+            # コンテスト会場脱出
+            *repeat((Hat.BTM, DEFAULT_DURATION, 0.5), 3),
+            *repeat((Hat.LEFT, DEFAULT_DURATION, 0.5), 10),
+            *repeat((Hat.BTM, DEFAULT_DURATION, 0.5), 8),
+            (Hat.BTM, DEFAULT_DURATION, 3.5),
+
+            # 船着き場まで
+            *repeat((Hat.LEFT, DEFAULT_DURATION, 0.5), 16),
+            *repeat((Hat.BTM, DEFAULT_DURATION, 0.5), 9),
+            *repeat((Hat.RIGHT, DEFAULT_DURATION, 0.5), 5),      
+            (Hat.TOP, DEFAULT_DURATION, 0.5),
+            (Hat.TOP, DEFAULT_DURATION, 2.5),
+
+            # 移動->会話->乗船
+            *repeat((Hat.LEFT, DEFAULT_DURATION, 0.5), 4),
+            *repeat((Hat.TOP, DEFAULT_DURATION, 0.5), 4),
+            (Button.A, DEFAULT_DURATION, 1.5),
+            (Button.A, DEFAULT_DURATION, 1.5),
+            (Button.A, DEFAULT_DURATION, 4.5),
+            *repeat((Button.A, DEFAULT_DURATION, 0.5), 4),
+            (Button.A, DEFAULT_DURATION, 8.5),
+            
+            # みなみのことう移動
+            *repeat((Hat.RIGHT, DEFAULT_DURATION, 0.5), 3),
+            *repeat((Hat.TOP, DEFAULT_DURATION, 0.5), 7),
+            *repeat((Hat.RIGHT, DEFAULT_DURATION, 0.5), 10),
+            *repeat((Hat.TOP, DEFAULT_DURATION, 0.5), 9),
+            *repeat((Hat.LEFT, DEFAULT_DURATION, 0.5), 6),
+            *repeat((Hat.BTM, DEFAULT_DURATION, 0.5), 2),
+            *repeat((Hat.LEFT, DEFAULT_DURATION, 0.5), 6),
+            *repeat((Hat.TOP, DEFAULT_DURATION, 0.5), 4),
+            (Hat.TOP, DEFAULT_DURATION, 2.5),
+            *repeat((Hat.TOP, DEFAULT_DURATION, 0.5), 6),
+        ])
+
+class Encounter():
     """エンカウント直前から、エンカウントする操作を定義するクラス
     """
+    def __init__(self, command: ImageProcPythonCommand):
+        self.__command = command
     def run(self):
-        pass
+        execute_sequence(self.__command, [
+            (Button.A, DEFAULT_DURATION, 15),
+            (Button.B, DEFAULT_DURATION, 5),
+        ])
 
 class PaintSeed(ImageProcPythonCommand):
     
@@ -61,12 +125,28 @@ class PaintSeed(ImageProcPythonCommand):
         super().__init__(cam, gui)
 
     def do(self):
+        """
+        事前条件:
+        - Zメニューのカーソルが「カートリッジこうかん」に合っている。
+        - ミナモシティのコンテスト会場の左端の絵画の前でセーブしている。
+        
+        個体情報:
+        
+        ```plaintext
+        フレームカウンタ: 0x661B(F) = 26139 = 7分15秒程度
+        待機: 65029(F) = 18分3秒程度
+        ひかえめ　めざ炎70
 
-        frame_until_seeing = 600
+        絵画seedずれ: 63F
+        エンカウントずれ: -511F
+        ```
+        """
+
+        frame_until_seeing = 26139
         calibration_see_picture = 63
 
-        frame_until_encountering = 600
-        calibration_encounter = 0
+        frame_until_encountering = 65029
+        calibration_encounter = -511
 
         operations = (
             Reset(self), 
---
+++ pokecon_extension.py
@@ -0,0 +1,95 @@
+from typing import List, Tuple, Union
+
+from Commands.PythonCommandBase import ImageProcPythonCommand
+from Commands.Keys import Button, Hat
+from SerialController.Commands.Keys import Direction
+
+# Pythonコマンド_作成How_to
+# - https://github.com/KawaSwitch/Poke-Controller/wiki/Python%E3%82%B3%E3%83%9E%E3%83%B3%E3%83%89_%E4%BD%9C%E6%88%90How_to
+
+#
+# pressメソッドの引数buttonが取りうる型
+#
+ButtonSpecifier = Union[
+    Button, 
+    Hat, 
+    Direction, 
+
+    # 同時入力
+    List[
+        Union[
+            Button, 
+            Hat, 
+            Direction
+        ]
+    ]
+]
+
+#
+# pressメソッドが取りうる引数の組み合わせ
+#
+# - buttons: 押下するボタン
+# - duration: 押下時間[秒] デフォルトは0.1
+# - wait: 押下後に待つ時間[秒] デフォルトは0.1
+#
+PressArgumentCombination = Union[
+    Tuple[ButtonSpecifier],
+    Tuple[ButtonSpecifier, float],
+    Tuple[ButtonSpecifier, float, float],
+]
+
+#
+# isContainTemplateメソッドが取りうる引数の組み合わせ
+#
+# - template_path: 探すテンプレート画像のパス(拡張子を含む画像名)
+# - threshold: しきい値 デフォルトでは0.7
+# - use_gray: 処理をグレースケールで行う デフォルトではTrue
+#
+IsContainTemplateArgumentCombination = Union[
+    Tuple[str],
+    Tuple[str, float],
+    Tuple[str, float, float],
+]
+
+ArgumentCombination = Union[
+    PressArgumentCombination,
+    IsContainTemplateArgumentCombination
+]
+
+def repeat(argument: ArgumentCombination, count: int) -> List[ArgumentCombination]:
+    return [argument] * count
+
+def execute_sequence(command: ImageProcPythonCommand, arguments: List[ArgumentCombination]):
+    """引数のリストを与えて、ImageProcPythonCommandの`press`メソッドと`isContainTemplate`メソッドを一括で実行する。
+    
+    `isContainTemplate`がFalseを返すと例外を送出します。おもに`press`が正常に完了しているかを確認するために使用する想定です。
+
+    Args:
+        command (ImageProcPythonCommand): ImageProcPythonCommandオブジェクト（`self`）
+        arguments (List[ArgumentCombination]): 各メソッドが取りうる引数の組み合わせのリスト
+    
+    ```python
+    def press(buttons: Button | Hat | Direction | list[Button | Hat | Direction], duration: float = 0.1, wait: float = 0.1):
+        pass
+
+    def isContainTemplate(template_path: str, threshold: float = 0.7, use_gray: bool = True):
+        pass
+    ```
+    
+    Raises:
+        Exception: `isContainTemplate`がFalseを返した
+    """
+    for argument in arguments:
+        if not _execute_method(command, argument):
+            raise Exception("isContainTemplateがFalseを返しました。")
+
+def _execute_method(command: ImageProcPythonCommand, argument: ArgumentCombination) -> bool:
+    
+    # ArgumentCombinationはいずれの場合も少なくとも1つの要素があり、
+    # IsContainTemplateArgumentCombinationの場合は、先頭の型は必ずstr
+
+    if type(argument[0]) is str:
+        return command.isContainTemplate(*argument)
+    else:
+        command.press(*argument)
+        return True
